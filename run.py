a4441bb157c2f63b11e30b4740332f01beed0fa6
Edward James Wells <edwardjameswells@gmail.com>
2024-12-16 04:56:28 +0000

    feat: Add function to print game rules

--- run.py
+++ run.py
@@ -1,3 +1,17 @@
+def print_rules():
+    print("\nThe fleet consists of 10 ships:")
+    print("  4 submarines  (1 field)")
+    print("  3 destroyers  (2 fields)")
+    print("  2 cruisers    (3 fields)")
+    print("  1 battleship  (4 fields)")
+    print("Ships cannot occupy contiguous fields.")
+    print("The game is won by sinking the whole fleet")
+    print("A ship is sunk by hitting all of its fields.")
+    print("There are 5 difficulty levels, level 1 being the easiest.")
+    print("At level 1 you start with 80 missiles, but for every higher level")
+    print("  the arsenal reduces by 10 missiles.\n")
+
+
 def new_session():
     """
     Begins a new session. Welcomes user, prompts for name, offers to display
