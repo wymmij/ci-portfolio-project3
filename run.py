c7818dc1c65f9b862c016aabb069f0e074e93325
Edward James Wells <edwardjameswells@gmail.com>
2024-12-16 04:51:50 +0000

    Initial commit. Add entry point function

--- run.py
+++ run.py
@@ -1,3 +1,14 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+def new_session():
+    """
+    Begins a new session. Welcomes user, prompts for name, offers to display
+    rules then starts the first game.
+    """
+    print("\nWelcome to Battleship.\n")
+
+    player = input("Enter your name:\n> ")
+
+    rules = input("Enter ? for the rules, otherwise any other key to continue:\n> ")
+    if rules == "?":
+        print_rules()
+
+    play_game()
